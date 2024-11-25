393bcabd6e1f7d3f0a2dd31be3cad71e8834ec34
haroontrailblazer <154034759+haroontrailblazer@users.noreply.github.com>
2024-11-25 20:31:56 +0530

    Create p11.py
    . Write a Python program using matplotlib to create a plot with two lines. Use the arrays y1 with 
    values [4, 9, 2, 13] and y2 with values [5, 3, 9, 11]. Label the x-axis as "Plotting Multiple Lines" and 
    display the plot with both lines.

---
+++ p11.py
@@ -0,0 +1,9 @@
+#Plotting multiple lines 
+import matplotlib.pyplot as dv 
+import numpy as np 
+y1 = np.array([4,9,2,13]) 
+y2 = np.array([5,3,9,11]) 
+dv.xlabel('Plotting Multiple Lines') 
+dv.plot(y1)
+dv.plot(y2) 
+dv.show()
