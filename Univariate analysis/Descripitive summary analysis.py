11e04ea040d1587539e5589aeb23af265a5ff140
haroontrailblazer <154034759+haroontrailblazer@users.noreply.github.com>
2024-11-26 19:08:01 +0530

    Add files via upload
    Summary Statistical Analysis
    This Python script performs a comprehensive statistical analysis on a given dataset. It leverages libraries such as pandas, numpy, scipy, and sklearn to compute various statistical metrics and transformations. The user inputs the file path of the dataset and the field name for analysis, and the program outputs detailed statistical information.
    
    Features
    Reading Data: The script reads data from a CSV file specified by the user.
    
    User Input: Prompts for the file path and the field name to analyze.
    
    Statistical Metrics:
    
    Mean
    
    Median
    
    Mode
    
    Standard Deviation
    
    Variance
    
    Range
    
    Skewness
    
    Kurtosis
    
    Quantiles (25th, 50th, 75th)
    
    Data Transformations:
    
    Min-Max Scaling
    
    Standardization
    
    Normalization
    
    How to Use
    Run the script in a Python environment.
    
    Enter the file path of your dataset when prompted.
    
    Enter the field name to analyze.
    
    The program will output the following:
    
    Original dataset values
    
    Mean, median, mode, standard deviation, variance, range, skewness, kurtosis, and quantiles
    
    Scaled data values
    
    Standardized data values
    
    Normalized data values
    
    Signed-off-by: haroontrailblazer <154034759+haroontrailblazer@users.noreply.github.com>

---
+++ Univariate analysis/Descripitive summary analysis.py
@@ -0,0 +1,48 @@
+import pandas as pd
+import numpy as np
+from scipy import stats
+from sklearn.preprocessing import scale
+from sklearn.preprocessing import minmax_scale
+
+
+print("\n                        SUMMARY STATISTICAL ANALYSIS")
+
+fp = (input("\nEnter your file path :"))
+df = pd.read_csv(fp,low_memory=False)
+col = str(input("Enter your field name :")) 
+ip = df[[col]].values.reshape(-1,1)
+
+
+print("\n                 Normal dataset :",ip)
+
+
+
+m = np.mean(ip)
+me = np.median(ip)
+mo = stats.mode(ip)
+sd = np.std(ip)
+var = np.var(ip)
+ra = np.ptp(ip)
+sk = stats.skew(ip)
+ku = stats.kurtosis(ip)
+qua = np.quantile(ip,0.25)
+qu = np.quantile(ip,0.50)
+c =np.quantile(ip,0.75)
+sca = minmax_scale(ip)
+stand = scale(ip)
+nor = ((ip-np.min(ip))/(np.max(ip)+np.min(ip)))
+
+print("\nMean value =",m)
+print("Median value =",me)
+print("Mode value =",mo)
+print("Standard deviation =",sd)
+print("Variance =",var)
+print("Range =",ra)
+print("Skewness value =",sk)
+print("Kurtosis value =",ku)
+print("Quantile of 25th =",qua)
+print("Quantile of 50th =",qu)
+print("Quantile of 75th =",c)
+print("\n                 Scaled data values :",sca)
+print("\n                 Standardized data values :",stand)
+print("\n                 Normalized data values",nor)
