c3d52c197d47bb988304220ff1ac7f46aee5c77c
haroontrailblazer <154034759+haroontrailblazer@users.noreply.github.com>
2024-11-26 15:59:00 +0530

    basemap
    15.4 Key Methods/objects of Basemap 
    List of all the key methods provided by the Basemap class in the mpl_toolkits.basemap module: 
    1. imshow(): Displays an image on the map. 
    2. Plot(): draw lines and/or markers 
    3. contour(): Draws contour lines on the map. 
    4. contourf(): Draws filled contour plots on the map. 
    5. pcolor(): draw a pseudocolor plot 
    6. pcolormesh(): Creates a pseudocolor plot with a non-regular rectangular grid. 
    7. scatter(): Plots points (scatter plot) on the map. 
    8. quiver(): Draws a vector field (arrows) on the map. 
    9. barbs(): Draws wind barbs on the map. 
    10. drawgreatcircle(): Plots a great circle (shortest path between two points) on the map.
    
    Signed-off-by: haroontrailblazer <154034759+haroontrailblazer@users.noreply.github.com>

--- Geographic plots/p1 basemap mercator plot.py
+++ Geographic plots/p1 basemap mercator plot.py
@@ -16,21 +16,3 @@ m.drawmeridians(range(-180, 180, 60), labels=[0,0,0,1])  # Label meridians on th
 # Show the map 
 plt.title("Simple Mercator Projection") 
 plt.show() 
-P43. Robinson Projection 
-from mpl_toolkits.basemap import Basemap 
-import matplotlib.pyplot as plt 
-# Create a simple map using the Robinson projection 
-m = Basemap(projection='robin', lon_0=0, resolution='c')
-# Draw map boundaries and features 
-m.drawcoastlines()
- m.drawcountries()
-                # Draw coastlines 
-                 # Draw country borders 
-m.fillcontinents(color='lightgreen', lake_color='aqua')  # Fill continents with color 
-m.drawmapboundary(fill_color='aqua')  # Draw boundary and fill oceans with color 
-# Draw parallels (latitude) and meridians (longitude) 
-m.drawparallels(range(-90, 90, 30), labels=[1,0,0,0])  # Label parallels on the left side 
-m.drawmeridians(range(-180, 180, 60), labels=[0,0,0,1])  # Label meridians on the bottom 
-# Show the map 
-plt.title("Simple Robinson Projection") 
-plt.show() 
