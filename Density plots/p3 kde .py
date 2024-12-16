6034674b8cfeaf0b908f52abfd4ca1e6fc4c5297
haroontrailblazer <154034759+haroontrailblazer@users.noreply.github.com>
2024-12-16 21:12:30 +0530

    commit

--- Density plots/p3 kde .py
+++ Density plots/p3 kde .py
@@ -1,7 +1,6 @@
 import matplotlib.pyplot as mp
 import seaborn as sp
 #createc a random number set fot the variable data
-data=[]
+data=[18, 20, 22, 21, 24, 30, 28, 27, 33, 35, 38, 40, 42, 45]
 sp.kdeplot(data,fill=True)
-mp.xlabel(cols[i-1])
 mp.show()
