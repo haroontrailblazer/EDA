a85025b629a75d04f76c170a09393c66e4e2d3ae
Haroon K M <154034759+haroontrailblazer@users.noreply.github.com>
2024-12-25 00:23:38 +0530

    cret: bubble chart with color

---
+++ Bubble chart/pg.2.py
@@ -0,0 +1,14 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+# Data
+np.random.seed(0)
+x = np.random.rand(10)
+y = np.random.rand(10)
+sizes = np.random.rand(10) * 1000
+colors = np.random.rand(10)
+# Bubble Chart with Color
+plt.subplot(2, 2, 2)
+plt.scatter(x, y, s=sizes, c=colors, alpha=0.5, cmap='viridis')
+plt.colorbar()
+plt.title('Bubble Chart with Color')
