e8d809df0689e98d7a4b730244ef80fe05296fff
Haroon K M <154034759+haroontrailblazer@users.noreply.github.com>
2024-12-25 21:34:59 +0530

    cret: using specified colors

---
+++ Tree maps/pg.2.py
@@ -0,0 +1,13 @@
+import matplotlib.pyplot as plt
+import squarify
+
+# Data
+sizes = [50, 25, 12, 6, 4, 3]
+labels = ['A', 'B', 'C', 'D', 'E', 'F']
+colors = ['red', 'green', 'blue', 'purple', 'orange', 'pink']
+
+# Plot
+plt.figure(figsize=(10, 6))
+squarify.plot(sizes=sizes, label=labels, color=colors, alpha=0.7)
+plt.axis('off')
+plt.show()
