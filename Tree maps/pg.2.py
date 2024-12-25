dd37adfd92a3494bb2449421ec20f2c610ef964a
Haroon K M <154034759+haroontrailblazer@users.noreply.github.com>
2024-12-25 21:45:07 +0530

    chore: edge color

--- Tree maps/pg.2.py
+++ Tree maps/pg.2.py
@@ -8,6 +8,6 @@ colors = ['red', 'green', 'blue', 'purple', 'orange', 'pink']
 
 # Plot
 plt.figure(figsize=(10, 6))
-squarify.plot(sizes=sizes, label=labels, color=colors, alpha=0.7)
+squarify.plot(sizes=sizes, label=labels, color=colors, alpha=0.7,edgecolor="black", linewidth=2)
 plt.axis('off')
 plt.show()
