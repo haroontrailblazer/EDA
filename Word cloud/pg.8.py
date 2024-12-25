64ac2d728b0064de1517d99cf283e93058f412dd
Haroon K M <154034759+haroontrailblazer@users.noreply.github.com>
2024-12-25 11:25:48 +0530

    cret: font styles

---
+++ Word cloud/pg.8.py
@@ -0,0 +1,10 @@
+from wordcloud import WordCloud
+import matplotlib.pyplot as plt
+
+text = "Quantum Computing"
+wordcloud = WordCloud(width=800, height=400, font_path='path/to/font.ttf').generate(text)
+
+plt.figure(figsize=(10, 5))
+plt.imshow(wordcloud, interpolation='bilinear')
+plt.axis('off')
+plt.show()
