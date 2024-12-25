8c95c149a3d3bb8590e0628d7c12f5b97acaeee7
Haroon K M <154034759+haroontrailblazer@users.noreply.github.com>
2024-12-25 21:36:23 +0530

    cret: nested values

---
+++ Tree maps/pg.4.py
@@ -0,0 +1,15 @@
+import matplotlib.pyplot as plt
+import squarify
+
+# Data
+sizes = [50, 25, 12, 6, 4, 3]
+labels = ['A', 'B', 'C', 'D', 'E', 'F']
+nested_sizes = [30, 20, 10, 5, 3, 2]
+nested_labels = ['A1', 'A2', 'B1', 'B2', 'C1', 'C2']
+
+# Plot
+plt.figure(figsize=(10, 6))
+squarify.plot(sizes=sizes, label=labels, alpha=0.7)
+squarify.plot(sizes=nested_sizes, label=nested_labels, alpha=0.5, color=['#ff9999','#66b3ff','#99ff99','#ffcc99','#c2c2f0','#ffb3e6'])
+plt.axis('off')
+plt.show()
