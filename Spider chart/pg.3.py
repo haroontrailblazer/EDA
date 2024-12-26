4cea6d0ab6c23c09bee8ea4aa0c00b5179adba80
Haroon K M <154034759+haroontrailblazer@users.noreply.github.com>
2024-12-26 22:54:08 +0530

    cret: linestyle with chart

---
+++ Spider chart/pg.3.py
@@ -0,0 +1,27 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+# Data
+labels = ['A', 'B', 'C', 'D', 'E']
+values = [4, 3, 2, 5, 4]
+
+# Number of variables
+num_vars = len(labels)
+
+# Compute angle of each axis
+angles = np.linspace(0, 2 * np.pi, num_vars, endpoint=False).tolist()
+
+# The plot is a circle, so we need to "complete the loop"
+values += values[:1]
+angles += angles[:1]
+
+# Plot
+fig, ax = plt.subplots(figsize=(6, 6), subplot_kw=dict(polar=True))
+ax.fill(angles, values, color='green', alpha=0.5)
+ax.plot(angles, values, color='green', linewidth=2, linestyle='dashed')
+ax.set_yticklabels([])
+
+ax.set_xticks(angles[:-1])
+ax.set_xticklabels(labels)
+
+plt.show()
