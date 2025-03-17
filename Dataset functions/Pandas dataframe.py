bf0bca553e4e52191714f6a313da963d8772d74d
Haroon K M <154034759+haroontrailblazer@users.noreply.github.com>
2025-03-17 11:06:08 +0530

    dataframe

--- Dataset functions/Pandas dataframe.py
+++ Dataset functions/Pandas dataframe.py
@@ -2,3 +2,6 @@ import pandas as pd
 df=pd.DataFrame({'reg.no':[621823243011,621823243012,621823243013,621823243014,621823243015,621823243016,621823243017,621823243018,621823243019,621823243020],
               'Name':['Arun','Bala','Chandru','Dinesh','Eswar','Fayaz','Ganesh','Hari','Irfan','Jagan'],'Rank':['a+','a','b+','b','c+','c','d+','d','e+','e']}).set_index('reg.no')
 print(df)
+print(df.loc[621823243011])
+print(df[df['Name']=='Arun'])
+print(df[df['Rank']=='a+'])
