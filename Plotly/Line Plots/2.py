cb280e67d708c75f731ddd19392e65d79f916ee5
Haroon K M <154034759+haroontrailblazer@users.noreply.github.com>
2025-03-28 20:07:38 +0530

    cmt

---
+++ Plotly/Line Plots/2.py
@@ -0,0 +1,13 @@
+import plotly.graph_objects as pg
+import numpy as np
+
+x=[110,120,130,140,150,160,170,180,190,200]
+y=np.random.randint(0, 500, 10)
+
+fig=pg.Figure(data=pg.Scatter(x=x,y=y))
+fig.update_layout(
+    title='Simple Line Plot',
+    xaxis_title='X-axis',
+    yaxis_title='Y-axis',
+    showlegend=True
+)
