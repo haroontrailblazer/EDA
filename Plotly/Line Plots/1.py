82cc073debb7a0be0beb15e107c2ca6a179249b4
Haroon K M <154034759+haroontrailblazer@users.noreply.github.com>
2025-03-28 20:00:45 +0530

    cmt

--- Plotly/Line Plots/1.py
+++ Plotly/Line Plots/1.py
@@ -1,1 +1,11 @@
+# import plotly
+import plotly.graph_objects as pg
+import numpy as np
 
+# define data
+days=[110,120,130,140,150,160,170,180,190,200]
+price=np.random.randint(1,100,10)
+
+# create a line plot
+fig=pg.Figure(data=pg.Scatter(x=days,y=price))
+fig.show()
