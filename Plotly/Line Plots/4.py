15b67c6ff67739134f44fa341bda0f82702103a7
Haroon K M <154034759+haroontrailblazer@users.noreply.github.com>
2025-03-28 20:29:34 +0530

    cmt

---
+++ Plotly/Line Plots/4.py
@@ -0,0 +1,11 @@
+import plotly.graph_objects as pg
+import numpy as np
+
+x = np.linspace(0, 1, 1000)
+y1 = np.sin(x)
+
+fg= pg.Figure(data=pg.Scatter(x=x,y=y1,name='sin(x)'))
+fg.update_layout(title='Line Plot with Shapes',xaxis_title='X-axis',yaxis_title='Y-axis')
+fg.add_annotation(text='sin(x)',x=0.5,y=0.5,showarrow=True,arrowhead=2,ax=20,ay=-30)
+fg.add_shape(type='rect',x0=0.2,x1=0.5,y0=-0.5,y1=0.5,fillcolor='yellow',opacity=0.2)
+fg.show()
